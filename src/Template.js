7013e3c7267479735e3775676644b487edd90afb
Rlilyyy <bingohlee@gmail.com>
2016-08-07 20:52:14 +0800

    一点修改，下午没心情写代码了……

--- src/Template.js
+++ src/Template.js
@@ -10,20 +10,22 @@ class Template {
   }
 
   getWarnTemplate(arg) {
-
+    let template = `<li class="console-list-item warn">${ arg }</li>`;
   }
 
   getInfoTemplate(arg) {
-
+    let template = `<li class="console-list-item info">${ arg }</li>`;
   }
 
   getErrorTemplate(arg) {
-
+    let template = `<li class="console-list-item error">${ arg }</li>`;
   }
 
   getTemplateByType(arg) {
     if (util.isObject(arg)) {
-
+      for (let [ key, value ] of Object.entries(arg)) {
+        
+      }
     } else if (util.isArray(arg)) {
 
     } else if (util.isFunction(arg) || util.isSymbol(arg)) {
--- src/Util.js
+++ src/Util.js
@@ -15,16 +15,16 @@ class Util {
    * 兼容性事件添加，attachEvent 无法接收 event 参数，并且 this 被绑定在 window 上，所以需要指定
    * @param  {Object} target     添加事件目标节点
    * @param  {String} event      添加事件名称
-   * @param  {Function} func     添加事件回调函数
+   * @param  {Function} callback 添加事件回调函数
    * @param  {Boolean} flag=false 事件是否在捕获阶段获取，默认为 false，仅在支持原生 addEventListener 的浏览器下有效
    * @return {void}
    */
-  static addEventListener(target, event, func, flag=false) {
+  static addEventListener(target, event, callback, flag=false) {
     if (target.addEventListener) {
-      target.addEventListener(event, func, flag);
+      target.addEventListener(event, callback, flag);
     } else if (target.attachEvent) {
       target.attachEvent(`on${ event }`, function() {
-        func.call(target, window.event);
+        callback.call(target, window.event);
       });
     } else {
       if (!!target[`on${ event }`]) {
@@ -32,10 +32,10 @@ class Util {
 
         target[`on${ event }`] = function(event) {
           oldEvent.call(this, event);
-          func.call(this, event);
+          callback.call(this, event);
         }
       } else {
-        target[`on${ event }`] = func;
+        target[`on${ event }`] = callback;
       }
     }
   }
